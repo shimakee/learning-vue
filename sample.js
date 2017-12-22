e4349951ceaa91950a5eb16e4669b77cd39f450b
shimakee <deleon.services@outlook.com>
2017-12-23 02:09:14 +0800

    Vue Component ways to create/register component

--- sample.js
+++ sample.js
@@ -1,7 +1,16 @@
+//register component first before creating root instance
 Vue.component('ken', {
     template: '<h1>Hello World!</h1>'
 });
 
+//variable created outside instance to use inside component
+var child = {template: '<h1>Again!</h1>'}
+
+//root instance created
 new Vue({
-    el: '#app'
+    el: '#app',
+    components:{
+        'kenn': child,
+        'kenneth': {template: '<h1>What?</h1>'} //component created directly inside instance
+    }
 });
