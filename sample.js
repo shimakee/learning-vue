75d9558450bafd23d9f91e35432e200d718ead71
shimakee <deleon.services@outlook.com>
2017-12-23 02:45:58 +0800

    Component - will only change per component

--- sample.js
+++ sample.js
@@ -1,8 +1,6 @@
-var data = {din: 'What?'};//putting data outside vue instance makes it the SAME DATA for all components
-
 Vue.component('ken', {
     data: function(){//must be a function if inside a component not a vue instance
-        return data//return object with data
+        return {din: 'What?'}//return object with data - will only change per component
     },
     template: '<h1>Hello {{ din }}! <button @click="changeSomething">Change</button></h1>',
     methods:{
