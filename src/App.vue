c703e71abf1132e42632a2dcebb4eac22656de12
shimakee <deleon.services@outlook.com>
2018-02-13 01:45:42 +0800

    commit to vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>Server status: <span class="status">{{msg}}</span></h1>
+    <h1>Server status: <span class="status">{{ msg }}</span></h1>
   </div> 
 </template>
 
