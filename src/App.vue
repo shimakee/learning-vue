c6f3cd7eadf21b270c6e88c366a84a9bb03b5997
shimakee <deleon.services@outlook.com>
2018-02-13 01:41:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>Server status: <span class="status">{{msg}}</span></h1>
+  </div> 
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Critical'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+.status{
+  color: red;
+}
+</style>
