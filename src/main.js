442315a8e439114339c310bd3751cdc3f9f554e0
shimakee <deleon.services@outlook.com>
2018-02-13 02:06:18 +0800

    COMPONENTS - creating .vue components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './Home.vue' //importing the Home component
+
+Vue.component('id-string', Home); //using the home component globaly
 
 new Vue({
   el: '#app',
