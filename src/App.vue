341c280c15177398e9e17c384227399c7eb1c032
shimakee <deleon.services@outlook.com>
2018-03-23 17:28:46 +0800

    components - singlefile - DIRECTIVES local - object passed in value

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
     <div class="container">
         <p v-directiveName>something</p> <!--creating a directive-->
         <p v-directiveName="'green'">something</p> <!--passing value to directive-->
-        <p v-directiveName:sampleWord="'green'">something</p> <!-- passing arguement for value in directive -->
-        <p v-directiveName.sampleMod="'red'">wait for it 4s..</p> <!-- passing arguement for value in directive -->
+        <p v-directiveName:modifierName="'green'">something</p> <!-- passing arguement for value in directive -->
+        <p v-directiveName.modifierName="'red'">wait for it 4s..</p> <!-- passing arguement for value in directive -->
+        <p v-directiveName.blink="'red'">*_*</p> <!-- passing arguement for value in directive -->
+        <p v-directiveName.blinker="{clr1:'green', clr2: 'blue'}">*_*</p> <!-- passing arguement for value in directive -->
     </div>
 </template>
 
@@ -14,23 +16,46 @@
                 bind(el, binding, vnode){
                     // el.style.color = 'red'; //dom/element manipulation
                     // el.style.color = binding.value; //binding value passed
-                    if(binding.modifiers['sampleMod']){
+                    if(binding.modifiers['modifierName']){
                     var delay = 4000;
                     }else{
                     var delay = 0;
                     }
+
                     setTimeout(function(){
-                    if(binding.arg == "sampleWord"){ //binding argument
-                        el.style.backgroundColor = binding.value;
-                        el.style.color = 'white';
-                    }else if(binding.value){
-                        el.style.color = binding.value;
-                    }else{
-                        el.style.color = 'white';
-                        el.style.backgroundColor = 'black';
-                    }
+
+                            if(binding.arg == "modifierName"){ //binding argument
+                                el.style.backgroundColor = binding.value;
+                                el.style.color = 'white';
+                            }else if(binding.value){
+                                el.style.color = binding.value;
+                            }else{
+                                el.style.color = 'white';
+                                el.style.backgroundColor = 'black';
+                            }
 
                     }, delay);
+
+                    if(binding.modifiers['blink'] || binding.modifiers['blinker']){ //created blink modifier
+                        if(binding.modifiers['blinker']){
+                            var clr1 = binding.value.clr1,//value passed is an object
+                                clr2 = binding.value.clr2;
+                        }else{
+
+                            var clr1 = 'blue',
+                            clr2 = 'red';
+                        }
+                        let interval = 1000;
+                        setInterval(function(){
+                            if(el.style.color == clr1){
+                                el.style.color = clr2
+                            }else{
+                                el.style.color = clr1
+                            }
+                        }, interval);
+                    }
+                    
+
                 }
            }
        }
