9cf37524498dff12bb678ad65cb519d4045ec1bc
shimakee <deleon.services@outlook.com>
2018-03-23 16:21:18 +0800

    components - singlefile - DIRECTIVES local

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,31 @@
 
 <script>
     export default {
-       
+       directives:{
+           'directiveName':{
+                bind(el, binding, vnode){
+                    // el.style.color = 'red'; //dom/element manipulation
+                    // el.style.color = binding.value; //binding value passed
+                    if(binding.modifiers['sampleMod']){
+                    var delay = 4000;
+                    }else{
+                    var delay = 0;
+                    }
+                    setTimeout(function(){
+                    if(binding.arg == "sampleWord"){ //binding argument
+                        el.style.backgroundColor = binding.value;
+                        el.style.color = 'white';
+                    }else if(binding.value){
+                        el.style.color = binding.value;
+                    }else{
+                        el.style.color = 'white';
+                        el.style.backgroundColor = 'black';
+                    }
+
+                    }, delay);
+                }
+           }
+       }
     }
 </script>
 
