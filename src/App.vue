45d64214a5580e2af87e6ebfbe7ab81e21482fb3
shimakee <deleon.services@outlook.com>
2018-03-26 16:36:20 +0800

    components - singlefile template - FILTERS - global & local

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
     <div class="container">
-        <h1>Custom modifier</h1>
-
-        <button v-ouch:mouseenter="mouseEnter"
-                v-ouch:mouseleave="mouseLeave"
-        >Click me</button>
+        <p>{{ sampleText | filterNameUsed }}</p> <!--using the one registered locally-->
+        <p>{{ nextText | filterName }}</p> <!--using the one registered Globally-->
     </div>
 </template>
 
 <script>
     export default {
-       directives:{
-           ouch:{
-               bind(el, binding, vnode){
-                //    el.onclick = function(){ //event listener
-                //        if(binding.arg == 'click'){
-                //            console.log(binding.value);
-                //             el.innerHTML = binding.value;
-                //         }
-                //    }
-
-                let evnt = binding.arg, //passing argument as event to listen to
-                    fn = binding.value //passing function as reference to execute
-
-                el.addEventListener(evnt, fn);
-               }
-           }
-       }
-       ,methods:{
-           mouseEnter:function(){
-               console.log('enter');
-           },
-           mouseLeave:function(){
-               console.log('leave');
+        data:function(){
+            return {sampleText:'the big brown thing',
+                nextText: 'ALL CAPS BABY!!'}
+        },
+       filters:{ //registering filter locally
+           filterNameUsed: function(value){
+               return value.toUpperCase();
            }
        }
     }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('filterName', function(value){ //registering filter globally
+  return value.toLowerCase();
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
